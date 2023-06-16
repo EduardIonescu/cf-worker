1d7b600bc79b9b3c66c43fa432716e2190800fa7
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2023-06-16 17:03:37 +0200

    send email

---
+++ functions/api/send-email.js
@@ -0,0 +1,55 @@
+const SibApiV3Sdk = require("@sendinblue/client");
+
+export async function onRequestPost({ request }) {
+	const contentType = request.headers.get("content-type");
+	if (!contentType.includes("application/json")) {
+		return new Response("Illegal Content Type", { status: 422 });
+	}
+
+	const data = await request.json();
+	const { lastName, firstName, email, phone, subject, message } = data;
+
+	const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+	apiInstance.setApiKey(
+		SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+		process.env.SENDINBLUE_API_KEY
+	);
+
+	let emailStatus;
+	const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+	sendSmtpEmail.htmlContent = `<p>
+	Last name: ${lastName} <br/>
+	First name: ${firstName}<br/>
+	Email: ${email}<br/>
+	Phone: ${phone || "None"}<br/>
+	Message: ${message}
+	</p>`;
+	sendSmtpEmail.subject = subject || "No subject";
+	sendSmtpEmail.sender = { email: "noreply@deepsign.de" };
+	sendSmtpEmail.to = [{ email: "eduardionescu23@gmail.com" }];
+
+	apiInstance
+		.sendTransacEmail(sendSmtpEmail)
+		.then(() => {
+			emailSent = true;
+		})
+		.catch((err) => {
+			emailStatus = err;
+		});
+
+	if (emailSent === true) {
+		return new Response(JSON.stringify({ message: "success" }), {
+			headers: {
+				"Content-Type": "application/json;charset=utf-8",
+			},
+		});
+	} else {
+		console.log(emailStatus);
+		return new Response(JSON.stringify({ message: "email error" }), {
+			headers: {
+				"Content-Type": "application/json;charset=utf-8",
+			},
+			status: 500,
+		});
+	}
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,34 @@
 import Link from "next/link";
-
+const formInput = {
+	lastName: "testo",
+	firstName: "hero",
+	email: "test@test.de",
+	phone: "",
+	subject: "hara",
+	message: "harahara",
+};
 export default function Home() {
+	const sendEmail = async () => {
+		console.log("tried sending");
+		const res = await fetch("api/send-email", {
+			body: JSON.stringify(formInput),
+			headers: {
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+		});
+		const { message } = await res.json();
+		if (message) {
+			console.log(message);
+			return;
+		}
+	};
 	return (
-		<main>
+		<main className="w-screen h-screen flex items-center flex-col justify-center">
 			<Link href="/posts">Posts</Link>
+			<button onClick={sendEmail} className="text-red-500 text-3xl">
+				Send Email
+			</button>
 		</main>
 	);
 }
