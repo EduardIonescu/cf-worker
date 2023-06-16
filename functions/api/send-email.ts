7ebb4a1099730a4decf0035c8f9470c18d4137b1
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2023-06-16 18:27:10 +0200

    .

--- functions/api/send-email.ts
+++ functions/api/send-email.ts
@@ -28,8 +28,6 @@ export async function onRequestPost({ request }: { request: any }) {
 	})
 		.then(() => (emailStatus = true))
 		.catch((err) => (emailStatus = err.message));
-	const { message2 } = await res.json();
-	const message3 = JSON.stringify(message2);
 	if (res.ok) {
 		return new Response(JSON.stringify({ message: "success" }), {
 			headers: {
@@ -38,7 +36,7 @@ export async function onRequestPost({ request }: { request: any }) {
 		});
 	} else {
 		console.log(emailStatus);
-		return new Response(JSON.stringify({ message: message3 }), {
+		return new Response(JSON.stringify({ message: emailStatus }), {
 			headers: {
 				"Content-Type": "application/json;charset=utf-8",
 			},
